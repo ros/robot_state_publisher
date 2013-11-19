b01dacf83a229ac0f11f5c27f6042dc495607182
Brian Jensen <Jensen.J.Brian@gmail.com>
2013-11-19 14:06:36 +0100

    Update KDL SegmentMap interface to use shared pointers

--- src/treefksolverposfull_recursive.cpp
+++ src/treefksolverposfull_recursive.cpp
@@ -44,7 +44,7 @@ int TreeFkSolverPosFull_recursive::JntToCart(const map<string, double>& q_in, ma
   // clear output
   p_out.clear();
 
-  addFrameToMap(q_in, p_out, tf::Stamped<KDL::Frame>(KDL::Frame::Identity(), ros::Time(), tree.getRootSegment()->second.segment.getName()), tree.getRootSegment(), flatten_tree);
+  addFrameToMap(q_in, p_out, tf::Stamped<KDL::Frame>(KDL::Frame::Identity(), ros::Time(), tree.getRootSegment()->second->segment.getName()), tree.getRootSegment(), flatten_tree);
 
   return 0;
 }
@@ -60,21 +60,21 @@ void TreeFkSolverPosFull_recursive::addFrameToMap(const map<string, double>& q_i
   // get pose of this segment
   tf::Stamped<KDL::Frame> this_frame;
   double jnt_p = 0;
-  if (this_segment->second.segment.getJoint().getType() != Joint::None){
-    map<string, double>::const_iterator jnt_pos = q_in.find(this_segment->second.segment.getJoint().getName());
+  if (this_segment->second->segment.getJoint().getType() != Joint::None){
+    map<string, double>::const_iterator jnt_pos = q_in.find(this_segment->second->segment.getJoint().getName());
     if (jnt_pos == q_in.end()){
       ROS_DEBUG("Warning: TreeFKSolverPosFull Could not find value for joint '%s'. Skipping this tree branch", this_segment->first.c_str());
       return;
     }
     jnt_p = jnt_pos->second;
   }
-  this_frame = tf::Stamped<KDL::Frame>(previous_frame * this_segment->second.segment.pose(jnt_p), ros::Time(), previous_frame.frame_id_);
+  this_frame = tf::Stamped<KDL::Frame>(previous_frame * this_segment->second->segment.pose(jnt_p), ros::Time(), previous_frame.frame_id_);
 
   if (this_segment->first != tree.getRootSegment()->first)
     p_out.insert(make_pair(this_segment->first, tf::Stamped<KDL::Frame>(this_frame, ros::Time(), previous_frame.frame_id_)));
 
   // get poses of child segments
-  for (vector<SegmentMap::const_iterator>::const_iterator child=this_segment->second.children.begin(); child !=this_segment->second.children.end(); child++){
+  for (vector<SegmentMap::const_iterator>::const_iterator child=this_segment->second->children.begin(); child !=this_segment->second->children.end(); child++){
     if (flatten_tree)
       addFrameToMap(q_in, p_out, this_frame, *child, flatten_tree);
     else
