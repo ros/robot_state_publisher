086699c73feec19c7e93aeb995fbe79ac8402da4
Paul Bovbel <pbovbel@clearpathrobotics.com>
2015-06-24 14:08:56 -0400

    Port to tf2 and enable using static broadcaster

--- include/robot_state_publisher/robot_state_publisher.h
+++ include/robot_state_publisher/robot_state_publisher.h
@@ -81,7 +81,6 @@ public:
 private:
   void addChildren(const KDL::SegmentMap::const_iterator segment);
 
-
   std::map<std::string, SegmentPair> segments_, segments_fixed_;
   const urdf::Model& model_;
   tf2_ros::TransformBroadcaster tf_broadcaster_;
