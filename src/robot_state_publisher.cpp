51a2192cda8be66ff2c9904a05690bc1c6b73ae4
Jasper Güldenstein <jasper.guel@hotmail.de>
2018-02-15 14:43:16 +0100

    added warning when joint is found in joint message but not in the joints specified by the urdf

--- src/robot_state_publisher.cpp
+++ src/robot_state_publisher.cpp
@@ -95,6 +95,9 @@ void RobotStatePublisher::publishTransforms(const map<string, double>& joint_pos
       tf_transform.child_frame_id = tf::resolve(tf_prefix, seg->second.tip);
       tf_transforms.push_back(tf_transform);
     }
+    else {
+      ROS_WARN_THROTTLE(10,"Joint with name: \"%s\" was in published in JointMessage but not found in URDF description", jnt->first.c_str());
+    }
   }
   tf_broadcaster_.sendTransform(tf_transforms);
 }
