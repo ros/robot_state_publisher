87e48c4f9082c1ae0b01cc7dee36ecc77c515b10
Chris Lalancette <clalancette@openrobotics.org>
2018-02-26 19:09:05 +0000

    Split out the actual node for robot_state_publisher to a separate file.
    This way, joint_state_publisher can actually be used successfully
    as a library.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- src/joint_state_listener.cpp
+++ src/joint_state_listener.cpp
@@ -153,37 +153,3 @@ void JointStateListener::callbackJointState(const JointStateConstPtr& state)
     }
   }
 }
-
-// ----------------------------------
-// ----- MAIN -----------------------
-// ----------------------------------
-int main(int argc, char** argv)
-{
-  // Initialize ros
-  ros::init(argc, argv, "robot_state_publisher");
-  ros::NodeHandle node;
-
-  // gets the location of the robot description on the parameter server
-  urdf::Model model;
-  if (!model.initParam("robot_description"))
-    return 1;
-
-  KDL::Tree tree;
-  if (!kdl_parser::treeFromUrdfModel(model, tree)) {
-    ROS_ERROR("Failed to extract kdl tree from xml robot description");
-    return 1;
-  }
-
-  MimicMap mimic;
-
-  for(std::map< std::string, urdf::JointSharedPtr >::iterator i = model.joints_.begin(); i != model.joints_.end(); i++) {
-    if(i->second->mimic) {
-      mimic.insert(make_pair(i->first, i->second->mimic));
-    }
-  }
-
-  JointStateListener state_publisher(tree, mimic, model);
-  ros::spin();
-
-  return 0;
-}
