451a0815d3e0196fd28e9c84471fdf73a9a5a3c4
William Woodall <william+github@osrfoundation.org>
2019-04-23 10:45:35 -0700

    avoid deprecated API (#18)
    Signed-off-by: William Woodall <william@osrfoundation.org>

--- src/robot_state_publisher.cpp
+++ src/robot_state_publisher.cpp
@@ -87,7 +87,7 @@ RobotStatePublisher::RobotStatePublisher(
   qos.durability = RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL;
 
   model_xml_.data = model_xml;
-  node_handle->set_parameter_if_not_set("robot_description", model_xml);
+  node_handle->declare_parameter("robot_description", model_xml);
   description_pub_ = node_handle->create_publisher<std_msgs::msg::String>(
     "robot_description", qos);
 }
