cd45a9620cda5f14535093b3936cfbd65aaa2566
Shane Loretz <sloretz@osrfoundation.org>
2020-04-15 10:23:21 -0700

    Remove unused tf.h that slipped through rebase
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- src/robot_state_publisher.cpp
+++ src/robot_state_publisher.cpp
@@ -40,7 +40,6 @@
 #include <kdl/frames_io.hpp>
 #include <geometry_msgs/TransformStamped.h>
 #include <tf2_kdl/tf2_kdl.h>
-#include <tf/tf.h>
 
 #include "robot_state_publisher/robot_state_publisher.h"
 
