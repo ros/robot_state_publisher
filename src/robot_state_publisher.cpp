1906b2e96bfd9f671f77e7cd83dcdad2a4d5ae3f
Russell Joyce <russell@russellj.co.uk>
2021-11-12 09:26:35 +0000

    Publish new fixed transforms when URDF is updated
    A new URDF may come with a new set of fixed transforms, so if the URDF
    is updated through the `robot_description` parameter, we also need to
    publish its new transforms.
    
    Fixes #181
    
    Signed-off-by: Russell Joyce <russell@russellj.co.uk>

--- src/robot_state_publisher.cpp
+++ src/robot_state_publisher.cpp
@@ -355,6 +355,7 @@ rcl_interfaces::msg::SetParametersResult RobotStatePublisher::parameterUpdate(
 
       try {
         setupURDF(new_urdf);
+        publishFixedTransforms();
       } catch (const std::runtime_error & err) {
         RCLCPP_WARN(get_logger(), "%s", err.what());
         result.successful = false;
