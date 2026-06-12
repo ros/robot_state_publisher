fb1831c40122b1998fd62248f976ed5bffccbb62
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-06-12 10:47:49 +0200

    CPP 20
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- src/robot_state_publisher.cpp
+++ src/robot_state_publisher.cpp
@@ -32,7 +32,6 @@
 
 #include <chrono>
 #include <fstream>
-#include <functional>
 #include <map>
 #include <memory>
 #include <stdexcept>
@@ -59,7 +58,7 @@ namespace robot_state_publisher
 namespace
 {
 
-inline bool check_valid_pub_freq(double val)
+constexpr bool check_valid_pub_freq(double val)
 {
   return val > 0.0 && val <= 1000.0;
 }
@@ -114,11 +113,15 @@ RobotStatePublisher::RobotStatePublisher(const rclcpp::NodeOptions & options)
     // Now that we have successfully declared the parameters and done all
     // necessary setup, install the callback for updating parameters.
     param_cb_ = add_on_set_parameters_callback(
-      std::bind(&RobotStatePublisher::parameterUpdate, this, std::placeholders::_1));
+      [this](const std::vector<rclcpp::Parameter> & parameters) {
+        return parameterUpdate(parameters);
+      });
 
     parameter_subscription_ = rclcpp::AsyncParametersClient::on_parameter_event(
       this->get_node_topics_interface(),
-      std::bind(&RobotStatePublisher::onParameterEvent, this, std::placeholders::_1));
+      [this](std::shared_ptr<const rcl_interfaces::msg::ParameterEvent> event) {
+        onParameterEvent(event);
+      });
   }
 
   // set publish frequency
@@ -144,7 +147,9 @@ RobotStatePublisher::RobotStatePublisher(const rclcpp::NodeOptions & options)
   joint_state_sub_ = this->create_subscription<sensor_msgs::msg::JointState>(
     "joint_states",
     rclcpp::SensorDataQoS(),
-    std::bind(&RobotStatePublisher::callbackJointState, this, std::placeholders::_1),
+    [this](sensor_msgs::msg::JointState::ConstSharedPtr state) {
+      callbackJointState(state);
+    },
     subscriber_options);
 
   publishFixedTransforms();
@@ -173,21 +178,21 @@ void RobotStatePublisher::setupURDF(const std::string & urdf_xml)
 
   // Initialize the mimic map
   mimic_.clear();
-  for (const std::pair<const std::string, urdf::JointSharedPtr> & i : model.joints_) {
-    if (i.second->mimic) {
+  for (const auto & [joint_name, joint] : model.joints_) {
+    if (joint->mimic) {
       // Just taking a reference to the model shared pointers ends up in a crash.
       // Explicitly make a copy of the JointMimic.
       auto jm = std::make_shared<urdf::JointMimic>();
-      jm->offset = i.second->mimic->offset;
-      jm->multiplier = i.second->mimic->multiplier;
-      jm->joint_name = i.second->mimic->joint_name;
-      mimic_[i.first] = jm;
+      jm->offset = joint->mimic->offset;
+      jm->multiplier = joint->mimic->multiplier;
+      jm->joint_name = joint->mimic->joint_name;
+      mimic_[joint_name] = jm;
     }
   }
 
   const KDL::SegmentMap & segments_map = tree.getSegments();
-  for (const std::pair<const std::string, KDL::TreeElement> & segment : segments_map) {
-    RCLCPP_DEBUG(get_logger(), "Got segment %s", segment.first.c_str());
+  for (const auto & [segment_name, element] : segments_map) {
+    RCLCPP_DEBUG(get_logger(), "Got segment %s", segment_name.c_str());
   }
 
   // walk the tree and add segments to segments_
@@ -252,11 +257,11 @@ void RobotStatePublisher::publishTransforms(
   std::vector<geometry_msgs::msg::TransformStamped> tf_transforms;
 
   // loop over all joints
-  for (const std::pair<const std::string, double> & jnt : joint_positions) {
-    std::map<std::string, SegmentPair>::iterator seg = segments_.find(jnt.first);
+  for (const auto & [joint_name, position] : joint_positions) {
+    auto seg = segments_.find(joint_name);
     if (seg != segments_.end()) {
       geometry_msgs::msg::TransformStamped tf_transform =
-        kdlToTransform(seg->second.segment.pose(jnt.second));
+        kdlToTransform(seg->second.segment.pose(position));
       tf_transform.header.stamp = time;
       tf_transform.header.frame_id = frame_prefix + seg->second.root;
       tf_transform.child_frame_id = frame_prefix + seg->second.tip;
@@ -277,12 +282,12 @@ void RobotStatePublisher::publishFixedTransforms()
 
   // loop over all fixed segments
   rclcpp::Time now = this->now();
-  for (const std::pair<const std::string, SegmentPair> & seg : segments_fixed_) {
-    geometry_msgs::msg::TransformStamped tf_transform = kdlToTransform(seg.second.segment.pose(0));
+  for (const auto & [joint_name, seg] : segments_fixed_) {
+    geometry_msgs::msg::TransformStamped tf_transform = kdlToTransform(seg.segment.pose(0));
     tf_transform.header.stamp = now;
 
-    tf_transform.header.frame_id = frame_prefix + seg.second.root;
-    tf_transform.child_frame_id = frame_prefix + seg.second.tip;
+    tf_transform.header.frame_id = frame_prefix + seg.root;
+    tf_transform.child_frame_id = frame_prefix + seg.tip;
     tf_transforms.push_back(tf_transform);
   }
   static_tf_broadcaster_->sendTransform(tf_transforms);
@@ -293,9 +298,10 @@ void RobotStatePublisher::callbackJointState(
 {
   if (state->name.size() != state->position.size()) {
     if (state->position.empty()) {
+      const char * first_joint = state->name.empty() ? "<none>" : state->name[0].c_str();
       RCLCPP_WARN(
         get_logger(), "Robot state publisher ignored a JointState message about joint(s) "
-        "\"%s\"(,...) whose position member was empty.", state->name[0].c_str());
+        "\"%s\"(,...) whose position member was empty.", first_joint);
     } else {
       RCLCPP_ERROR(get_logger(), "Robot state publisher ignored an invalid JointState message");
     }
@@ -335,11 +341,10 @@ void RobotStatePublisher::callbackJointState(
       joint_positions.emplace(state->name[i], state->position[i]);
     }
 
-    for (const std::pair<const std::string, urdf::JointMimicSharedPtr> & i : mimic_) {
-      if (joint_positions.find(i.second->joint_name) != joint_positions.end()) {
-        double pos = joint_positions[i.second->joint_name] * i.second->multiplier +
-          i.second->offset;
-        joint_positions.emplace(i.first, pos);
+    for (const auto & [mimic_name, mimic] : mimic_) {
+      if (auto it = joint_positions.find(mimic->joint_name); it != joint_positions.end()) {
+        const double pos = it->second * mimic->multiplier + mimic->offset;
+        joint_positions.emplace(mimic_name, pos);
       }
     }
 
